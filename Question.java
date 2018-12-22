8862e46dc96e3567eb8c55635ba96d6202dda358
marwanaboelnaga <marwan.aboelnaga@gmail.com>
2018-12-22 20:57:03 +0200

    classes

---
+++ Question.java
@@ -0,0 +1,11 @@
+public class Question {
+    int qId;
+    String q;
+    public Question(int qId , String q )
+    {
+        this.qId = qId;
+        this.q=q;
+    }
+    public String getQues() { return q; }
+}
+
